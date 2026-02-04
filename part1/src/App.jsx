ee67e7676415df2c876f3d5523c957e2c464d574
Skywokerr <atolwamichael7@gmail.com>
2026-02-04 13:05:29 +0300

    tinkering with the app.jsx file

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => {
+const Hello  = (props) => {
+    console.log(props)
     return (
         <div>
-            <p>Hello world</p>
+            <p>
+                Hello {props.name}, you are {props.age} years old
+            </p>
+        </div>
+    )
+}
+
+
+const App = () => {
+    const name = 'Peter'
+    const age = 10
+    
+    return (
+        <div>
+            <p>Greeting</p>
+            <Hello name = 'Maya' age={26+10} />
+            <Hello name={name} age={age} />
         </div>
     )
 }
